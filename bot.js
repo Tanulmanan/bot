602156a1adfcdd22e6815a3d0679d8742e9f6a5b
Samuel Adekunle <ebunsamuel@yahoo.com>
2020-08-04 15:00:48 +0100

    got to payment page

--- bot.js
+++ bot.js
@@ -1,5 +1,21 @@
+// external library imports
 require("dotenv").config();
 const puppeteer = require("puppeteer");
+
+// user agent data
+const userData = {
+  email: process.env.EMAIL,
+  password: process.env.PASSWORD,
+  cvc: process.env.CVC,
+};
+
+// scroll to selector helper function
+const scrollToSelector = async (page, selector) => {
+  await page.waitForSelector(selector);
+  await page.evaluate((selector) => {
+    document.querySelector(selector).scrollIntoView();
+  }, selector);
+};
 
 // main flow
 const main = async () => {
@@ -11,164 +27,97 @@ const main = async () => {
 
   const page = await browser.newPage();
 
-  const data = {
-    firstName: process.env.FIRST_NAME,
-    lastName: process.env.LAST_NAME,
-    phone: process.env.PHONE,
-    email: process.env.EMAIL,
-    address: {
-      line: process.env.ADDRESS_LINE,
-      city: process.env.CITY,
-      postCode: process.env.POSTCODE,
-    },
-    payment: {
-      card: process.env.CARD,
-      date: process.env.EXPIRY_DATE,
-      cvc: process.env.CVC,
-    },
-  };
-
   // go to url
-  await gotoURL(page, process.env.URL);
+  await page.goto(process.env.URL, { waitUntil: "networkidle0" });
 
-  // scoll to size selector
-  await scrollToSelector(page, ".add-to-cart-form");
+  // scoll to size
+  await scrollToSelector(page, ".buying-tools-container");
 
   // click on size
-  await pickSize(page, process.env.SIZE);
+  await async function () {
+    await page.evaluate((_size) => {
+      const sizes = Array.from(document.querySelectorAll(".size-layout > li"));
+      for (const size of sizes) {
+        const btn = size.firstElementChild
+        if (btn.innerHTML === _size) {
+          btn.click();
+        }
+      }
+    }, process.env.SIZE);
+  }()
 
   // scroll to add to cart
-  await scrollToSelector(page, ".add-to-cart-btn");
+  await scrollToSelector(page, "button[data-qa=add-to-cart]");
 
-  // click on add to cart then checkout
-  await addToCart(page);
-  page.waitFor(2000);
-
-  // scroll to checkout button
-  await scrollToSelector(page, "button[data-automation=go-to-checkout-button]");
-
-  // click on checkout button
-  await checkout(page);
-
-  // fill guest form
-  await fillForm(page, data);
-
-  // place order
-  // NOTE - make sure to debug with BROWSER=YES because might need pre payment approval
-  await page.waitForSelector("button.placeOrderBtn");
-  if (process.env.BUY === "YES") {
+  // click on add to cart
+  await async function () {
     await page.evaluate(() => {
-      document.querySelector("button.placeOrderBtn").click();
-    });
-  } else {
+      const btn = document.querySelector("button[data-qa=add-to-cart]")
+      btn.click()
+    })
+  }()
+
+  // wait for pop-up
+  await page.waitForSelector("button[data-qa=checkout-link]");
+
+  // click on checkout
+  await async function () {
     await page.evaluate(() => {
-      document.querySelector("button.placeOrderBtn").innerHTML =
-        "CLICK HERE TO BUY";
-    });
-  }
+      const btn = document.querySelector("button[data-qa=checkout-link]")
+      btn.click()
+    })
+  }()
+
+  // wait for sign in form
+  await page.waitForSelector(".nike-unite-form");
+
+  // fill sign in form 
+  await async function () {
+    await page.type("input[data-componentname=emailAddress]", userData.email, { delay: 50 });
+    await page.type("input[data-componentname=password]", userData.password, { delay: 50 });
+  }()
+
+  // click keep me signed in
+  await async function () {
+    await page.evaluate(() => {
+      const btn = document.querySelector("#keepMeLoggedIn > input")
+      btn.click()
+    })
+  }()
+
+  // click on sign in form
+  await async function () {
+    await page.evaluate(() => {
+      const btn = document.querySelector(".nike-unite-submit-button")
+      btn.firstElementChild.click()
+    })
+  }()
+
+  // wait for shipping
+  await page.waitForSelector("#shipping");
+  await scrollToSelector(page, ".continuePaymentBtn");
+
+  // click on continue payment button
+  await async function () {
+    await page.evaluate(() => {
+      const btn = document.querySelector(".continuePaymentBtn")
+      btn.click()
+    })
+  }()
+
+  // scroll to payment
+  await scrollToSelector(page, "#payment")
+
+  // fill in CVC
+  await async function () {
+    const elementHandle = await page.$(".credit-card-iframe-cvv");
+    const frame = await elementHandle.contentFrame();
+    await frame.type("input#cvNumber", userData.cvc, { delay: 50 });
+  }()
 
   // wait for a minute before closing browser
   await page.waitFor(60 * 1000);
   await browser.close();
-};
-
-const gotoURL = async (page, url) => {
-  await page.goto(url, { waitUntil: "networkidle0" });
-};
-
-const scrollToSelector = async (page, selector) => {
-  await page.waitForSelector(selector);
-  await page.evaluate((selector) => {
-    document.querySelector(selector).scrollIntoView();
-  }, selector);
-};
-
-const pickSize = async (page, size) => {
-  await page.evaluate((size) => {
-    const sizes = Array.from(
-      document.querySelectorAll("#buyTools > div > fieldset > div > div")
-    );
-    const desiredSize = sizes.find((s) => {
-      return s.children[1].innerHTML === size;
-    });
-    desiredSize.children[1].click();
-  }, size);
-};
-
-const addToCart = async (page) => {
-  await page.evaluate(() => {
-    document.querySelector(".add-to-cart-btn").click();
-  });
-  const checkoutSelector = "button[data-test=qa-cart-checkout]";
-  await page.waitForSelector(checkoutSelector);
-  await page.evaluate((selector) => {
-    document.querySelector(selector).click();
-  }, checkoutSelector);
-};
-
-const checkout = async (page) => {
-  await page.evaluate(() => {
-    document
-      .querySelector("button[data-automation=go-to-checkout-button]")
-      .click();
-  });
-  const guestCheckoutSelector = "button[data-automation=guest-checkout-button]";
-  await page.waitForSelector(guestCheckoutSelector);
-  await page.evaluate((selector) => {
-    document.querySelector(selector).click();
-  }, guestCheckoutSelector);
-};
-
-const fill = async (page, selector, data) => {
-  await page.type(selector, data, { delay: 100 });
-};
-
-const fillForm = async (
-  page,
-  { firstName, lastName, email, phone, address, payment }
-) => {
-  await page.waitForSelector("#shipping");
-
-  // wait for inputs to load up
-  await page.waitFor(2000);
-
-  await page.evaluate(() => {
-    document.querySelector("a#addressSuggestionOptOut").click();
-  });
-
-  await page.waitFor(2000);
-
-  await fill(page, "input#firstName", firstName);
-  await fill(page, "input#lastName", lastName);
-  await fill(page, "input#email", email);
-  await fill(page, "input#phoneNumber", phone);
-  // NOTE - make sure nike likes the address
-  await fill(page, "input#address1", address.line);
-  await fill(page, "input#city", address.city);
-  await fill(page, "input#postalCode", address.postCode);
-
-  await page.evaluate(() => {
-    document.querySelector("button.saveAddressBtn").click();
-  });
-
-  await page.waitFor(2000);
-
-  await page.evaluate(() => {
-    document.querySelector("button.continuePaymentBtn").click();
-  });
-
-  await page.waitFor(2000);
-  await scrollToSelector(page, ".credit-card-iframe");
-  const elementHandle = await page.$(".credit-card-iframe");
-  const frame = await elementHandle.contentFrame();
-  await fill(frame, "input#creditCardNumber", payment.card);
-  await fill(frame, "input#expirationDate", payment.date);
-  await fill(frame, "input#cvNumber", payment.cvc);
-
-  await page.waitFor(2000);
-  await page.evaluate(() => {
-    document.querySelector("button.continueOrderReviewBtn").click();
-  });
 };
 
 main();
